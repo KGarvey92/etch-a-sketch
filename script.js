1a0d077975fe4843e47a8ba824d912c69f953932
KGarvey92 <kgarvey92@gmail.com>
2022-08-05 18:21:24 +0800

    add ability to create and destroy grid

--- script.js
+++ script.js
@@ -13,16 +13,23 @@ function createGrid(gridSize) {
     }
 }
 
+function destroyGrid() {
+    document.querySelectorAll('.grid-item').forEach(e => e.remove());
+}
+
 // add event listener on button
 const gridBtn = document.querySelector('.gridBtn');
-gridBtn.addEventListener
+gridBtn.addEventListener('click', () => {
+    // remove previous grid
+    destroyGrid();
     // prompt user for number between 1-100
-
-
-    // check number is valid and an integer
-
-
+    let input = 0;
+    while (input < 1 || input > 100) {
+        input = parseInt(prompt('Enter a number between 1-100'));
+    }
     // change cssGrid according to input
 
 
     // rerun createGrid function
+     createGrid(input);
+})
